0b1d986a0906dd34c016db24192e86785969f0b2
karinevicr <karine.paixao@sou.inteli.edu.br>
2024-10-31 22:54:48 -0300

    fix: arruma distancia entre os nomes

--- congresso-icb/src/Speaker.js
+++ congresso-icb/src/Speaker.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Speaker = ({ name, university }) => (
   <div style={{ textAlign: 'center'}}>
-    <h3 style={{ fontSize: '1.2em', fontWeight: 'inter' }}>{name}</h3>
-    <p style={{ fontStyle: 'italic', fontWeight: 'inter', color: '#000' }}>{university}</p>
+    <h3 style={{ fontSize: '1.2em', fontWeight: 'inter', marginBottom: '5px' }}>{name}</h3>
+    <p style={{ fontStyle: 'italic', fontWeight: 'inter', color: '#000', marginTop: '0' }}>{university}</p>
   </div>
 );
 
